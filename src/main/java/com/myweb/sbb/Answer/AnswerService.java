583f63056c57d6c37d14db0e8d2acceb61d565ef
85307 <dnjs5397@naver.com>
2023-05-15 13:00:59 +0900

    게시글, 댓글 수정/삭제 기능 구현

--- src/main/java/com/myweb/sbb/Answer/AnswerService.java
+++ src/main/java/com/myweb/sbb/Answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.myweb.sbb.Answer;
 
+import com.myweb.sbb.DataNotFoundException;
 import com.myweb.sbb.Question.Question;
 import com.myweb.sbb.User.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,25 @@ public class AnswerService {
         answer.setLocalDateTime(LocalDateTime.now());
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        }
+        else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
